695294ad13db5417f34487aaf957c3d820a2c864
Rasel Mahmud <raselm606@gmail.com>
2026-02-07 02:37:51 +0600

    last changes for today

--- components/sections/OurJourney.jsx
+++ components/sections/OurJourney.jsx
@@ -7,7 +7,7 @@ const OurJourney = () => {
         <div className="container cline left_100">
           <div className="row">
             <div className="col-lg-4">
-              <div className="journey_content" data-aos="fade-up" data-aos-delay="600">
+              <div className="journey_content"  >
                 <h2 className="section_title_white">Our journey over the years 
                    <br /> reflects long-term vision  <br /> and progress</h2> 
                 </div>
@@ -15,31 +15,31 @@ const OurJourney = () => {
           </div>
 
           <div className="timeline_area">
-            <Image data-aos="fade-up" data-aos-delay="600" className="line_img" src={timelineImage} alt="timeline" />
+            <Image   className="line_img" src={timelineImage} alt="timeline" />
             <div className="row justify-content-center  mt-5">
               <div className="col-lg-2">
-                  <div className="timeline_item timeline_one" data-aos="fade-up" data-aos-delay="700">
+                  <div className="timeline_item timeline_one"  >
                     <h4>Founding Year</h4>
                     <p>Laying the foundation 2019</p>
                   </div>
               </div>
 
               <div className="offset-lg-1 col-lg-2">
-                  <div className="timeline_item timeline_two" data-aos="fade-up" data-aos-delay="800">
+                  <div className="timeline_item timeline_two"  >
                     <h4>Year 3</h4>
                     <p>Building the Ecosystem</p>
                   </div>
               </div>
 
               <div className="offset-lg-1 col-lg-2">
-                  <div className="timeline_item timeline_three" data-aos="fade-up" data-aos-delay="900">
+                  <div className="timeline_item timeline_three"  >
                     <h4>Year 5</h4>
                     <p>Expansion & Market Validation</p>
                   </div>
               </div>
 
               <div className="offset-lg-1 col-lg-2"> 
-                  <div className="timeline_item timeline_four" data-aos="fade-up" data-aos-delay="950">
+                  <div className="timeline_item timeline_four"  >
                     <h4>Today</h4>
                     <p>Focused on private expansion</p>
                   </div> 
