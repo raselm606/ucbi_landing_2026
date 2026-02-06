d27affc070446fafaa24da74acb36ff7b6a827ae
Rasel Mahmud <raselm606@gmail.com>
2026-02-07 01:32:47 +0600

    new section add image

--- components/sections/GoalandEffort.jsx
+++ components/sections/GoalandEffort.jsx
@@ -1,9 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
-import goalImg from '../../public/images/goal.jpg';
-import azbitf from '../../public/images/margin.png';
-import yahoof from '../../public/images/yahoo.png';
-import zoomexf from '../../public/images/zoomex.png';
+import goalImg from '../../public/images/goal1.jpg';
+import yahoof from '../../public/images/yahoo1.png';
 const GoalandEffort = () => {
   return (
     <>
@@ -13,17 +11,15 @@ const GoalandEffort = () => {
                 <div className="col-lg-6 mb-3 ">
                     <div className="goal_content bg_bb">
                         <h2 className="section_title_dark" data-aos="fade-up" data-aos-delay="700">
-                            Main Goal & Efforts support <br /> secure digital innovation.
+                            Long-term support <br /> for secure digital innovation
                         </h2>
-                        <p data-aos="fade-up" data-aos-delay="750" className="section_paragraph_dark">We deliver transparent, global financial solutions bridging traditional finance and digital assets, driving user-centric innovation and collaborative growth in decentralized finance.</p>
+                        <p data-aos="fade-up" data-aos-delay="750" className="section_paragraph_dark">Operating within a private and confidential framework UCBI is structured as a holding company supporting selected digital and blockchain-related projects through private equity-style shareholding</p>
 
                         <div className="divider_lin"></div>
 
                         <div className="featured_o" data-aos="fade-up" data-aos-delay="800">
-                            <p>We are featured on:</p>
-                            <Link href="#" className="fe_link"><Image src={yahoof} alt="featured logos" width={100} height={50} /></Link>
-                            <Link href="#" className="fe_link"><Image src={azbitf} alt="featured logos" width={100} height={50} /></Link>
-                            <Link href="#" className="fe_link"><Image src={zoomexf} alt="featured logos" width={100} height={50} /></Link>
+                            <p>Market Data:</p>
+                            <Link href="#" className="fe_link"><Image src={yahoof} alt="featured logos"   /></Link> 
                              
                         </div>
                     </div>
